ad4cc462b4f60a6f7859aee0330ea20132672e58
Aji Setiawan <ajiisetiawan09@gmail.com>
2025-01-22 11:24:27 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require("path")
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express()
+
+//connect to mongodb
+mongoose.connect("mongodb://127.0.0.1/shop_db")
+    .then((result) => {
+        console.log("Connect to mongodb")
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(3000, () => {
+    console.log("shop app listen on port http://localhost:3000")
+})
+
+
+
